7dacbbbfa2880b44552ad615f88039e0ae04f51e
AgonAvdimetaj <agonavdimetaj@gmail.com>
2022-03-04 11:00:37 +0100

    Lint code

--- block.test.js
+++ block.test.js
@@ -4,21 +4,17 @@ describe('Block', () => {
     let data, lastBlock, block;
 
     beforeEach(() => {
-data = 'Gjija';
-lastBlock = Block.genesis();
-block = Block.mineBlock(lastBlock, data);
-
+        data = 'Gjija';
+        lastBlock = Block.genesis();
+        block = Block.mineBlock(lastBlock, data);
     });
 
-it('sets the `data` to match the input', () => {
-    expect(block.data).toEqual(data);
-});
+    it('should set the `data` to match the input', () => {
+        expect(block.data).toEqual(data);
+    });
 
-it('sets the `lastHash` to match the has of the last block', () => {
-
-    expect(block.lastHash).toEqual(lastBlock.hash);
-
-
-});
+    it('should set the `lastHash` to match the has of the last block', () => {
+        expect(block.lastHash).toEqual(lastBlock.hash);
+    });
 
 });
