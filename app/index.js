c499fff43d4b31d0ffddf7836997480477bfca6b
AgonAvdimetaj <agonavdimetaj@gmail.com>
2022-03-10 20:12:23 +0100

    Blockchain Network Peers

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
+
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 app.get('/blocks', (req, res) => {
@@ -24,3 +27,6 @@ app.post('/mine', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
+
+//HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
