477a5831390fa618e0a72c18f5f88c2e7882a25a
AgonAvdimetaj <agonavdimetaj@gmail.com>
2022-03-10 19:32:10 +0100

    Mine blocks post request

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -6,20 +7,20 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
-
+app.use(bodyParser.json());
 app.get('/blocks', (req, res) => {
-
-bc.addBlock('Esat');
-bc.addBlock('Enis');
-bc.addBlock('Dorjan');
-bc.addBlock('Dren');
-
 
     res.json(bc.chain);
     
 });
 
 
+app.post('/mine', (req, res) => {
 
+    const block = bc.addBlock(req.body.data);
+    console.log('New block added: ${block.toString()}');
+    res.redirect('/blocks');
+
+});
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
