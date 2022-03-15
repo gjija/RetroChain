4f6fb6e7a332b6e83f209b286ca4f47006121d3c
AgonAvdimetaj <agonavdimetaj@gmail.com>
2022-03-15 19:34:59 +0100

    get transactions

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require ('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet ();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -24,6 +28,12 @@ app.post('/mine', (req, res) => {
     console.log(`New block added: ${block.toString()}`);
     p2pServer.syncChains();
     res.redirect('/blocks');
+
+});
+
+app.get('/transactions', (req, res) => {
+
+    res.json(tp.transactions);
 
 });
 
