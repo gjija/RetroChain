9c54b4e628f774ec417a5d827b2510083b74ef96
AgonAvdimetaj <agonavdimetaj@gmail.com>
2022-03-15 18:44:09 +0100

    transaction pool test

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,38 @@
+const TransactionPool = require('./transaction-pool');
+const Transaction = require ('./transaction');
+const Wallet = require ('./index');
+
+
+describe('TransactionPool', () => {
+
+let tp, wallet, transaction;
+
+beforeEach(() => {
+
+tp = new TransactionPool();
+wallet = new Wallet();
+transaction = Transaction.newTransaction(wallet, 'GjijaBiqGenesis', 30);
+tp.updateOrAddTransaction(transaction);
+
+
+});
+
+it('adds a transaction to the pool', () => {
+
+expect(tp.transactions.find(t => t.id === transaction.id)).toEqual(transaction);
+
+});
+
+it('updates a transaction in the pool', () => {
+
+    const oldTransaction = JSON.stringify(transaction);
+    const newTransaction = transaction.update(wallet, 'GjijaBiqGenesis', 40);
+    tp.updateOrAddTransaction(newTransaction);
+
+    expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id))).not.toEqual(oldTransaction);
+
+
+
+});
+
+});
